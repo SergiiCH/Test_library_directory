0c4a7af0ca8d7f09c55aaf3307ff5b581786863b
SergiiCH <poshtovaskrinka.s2017@ukr.net>
2020-09-19 23:45:43 +0300

    Добавьте файлы проекта.

---
+++ Test_library_directory/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Test_library_directory.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
